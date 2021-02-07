6b397fd6e8bc749c5da750db5b90deeb6bf9dcc4
daxitzadafiya <17bmiit040@gmail.com>
2021-02-07 21:31:46 +0530

    recreate inner  div

---
+++ src/components/inputForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-container fluid>
+      <table>
+        <tr>
+          <td>
+            <label for="CustName" class="font-weight-bold"
+              >CUSTOMER NAMES :</label
+            >
+          </td>
+          <td class="mb-5">
+            <b-form-input
+              id="CustName"
+              placeholder="enter customer name"
+            ></b-form-input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="CustName" class="font-weight-bold"
+              >PO / JOB NAME :</label
+            >
+          </td>
+          <td>
+            <b-form-input
+              id="CustName"
+              placeholder="enter po / job name"
+            ></b-form-input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="CustName" class="font-weight-bold">UNIT #</label>
+          </td>
+          <td>
+            <b-form-input id="CustName" placeholder="enter unit"></b-form-input>
+          </td>
+        </tr>
+      </table>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  }
+};
+</script>
---
+++ src/components/mainGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-div">
+    <innerDiv />
+    <innerDiv />
+    <innerDiv />
+  </div>
+</template>
+
+<script>
+import innerDiv from "./innerDiv";
+export default {
+  components: {
+    innerDiv
+  }
+};
+</script>
+
+<style>
+.main-div {
+  display: flex;
+  padding: 30px 30px;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  top: 72px;
+}
+</style>
